5c8a2c99d5d12afc41d11d7fc650215036adcdb9
BartoszStachowski <stachowskibartosz96@gmail.com>
2026-03-21 10:03:54 +0100

    Multi-Layered Authorization

--- app/(shared-layout)/blog/[postId]/page.tsx
+++ app/(shared-layout)/blog/[postId]/page.tsx
@@ -10,6 +10,7 @@ import CommentSection from '@/components/web/CommentSection';
 import { Metadata } from 'next';
 import PostPresence from '@/components/web/PostPresence';
 import { getToken } from '@/lib/auth-server';
+// import { redirect } from 'next/navigation';
 
 interface PostIdRouteProps {
   params: Promise<{
@@ -44,6 +45,10 @@ const PostIdRoute = async ({ params }: PostIdRouteProps) => {
     await preloadQuery(api.comments.getCommentsByPostId, { postId: postId }),
     await fetchQuery(api.presence.getUserId, {}, { token }),
   ]);
+
+  // if (!userId) {
+  //   return redirect('/auth/login');
+  // }
 
   if (!post) {
     return (
