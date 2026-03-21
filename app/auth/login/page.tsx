d0ad12e266140c52ea3b771938e780fc0cbca4ad
BartoszStachowski <stachowskibartosz96@gmail.com>
2026-03-21 10:02:01 +0100

    error message

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,29 +1,29 @@
-"use client";
+'use client';
 
-import { loginSchema } from "@/app/schemas/auth";
-import { Button } from "@/components/ui/button";
+import { loginSchema } from '@/app/schemas/auth';
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
+} from '@/components/ui/card';
 import {
   Field,
   FieldError,
   FieldGroup,
   FieldLabel,
-} from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
-import { authClient } from "@/lib/auth-client";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Loader2 } from "lucide-react";
-import { useRouter } from "next/navigation";
-import { useTransition } from "react";
-import { Controller, useForm } from "react-hook-form";
-import { toast } from "sonner";
-import z from "zod";
+} from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+import { authClient } from '@/lib/auth-client';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useTransition } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+import z from 'zod';
 
 const LoginPage = () => {
   const [isPending, startTransition] = useTransition();
@@ -32,8 +32,8 @@ const LoginPage = () => {
   const form = useForm({
     resolver: zodResolver(loginSchema),
     defaultValues: {
-      email: "",
-      password: "",
+      email: '',
+      password: '',
     },
   });
 
@@ -44,11 +44,14 @@ const LoginPage = () => {
         password: data.password,
         fetchOptions: {
           onSuccess: () => {
-            toast.success("Logged in successfully");
-            router.push("/");
+            toast.success('Logged in successfully');
+            router.push('/');
           },
           onError: (error) => {
-            toast.error(error.error.message);
+            const errMsg =
+              error.error.message ??
+              'Invalid credentials or user not authenticated';
+            toast.error(errMsg);
           },
         },
       });
